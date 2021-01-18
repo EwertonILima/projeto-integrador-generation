b834136731f7ddec9e2a2b36f9e4696116437e89
jorgehauck <jorge.hauck1997@outlook.com>
2021-01-18 12:24:37 -0300

    Criação do DER Banco de Dados

---
+++ ArquivosSql/Projeto Integrador_mysql_create.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `tb_categoria` (
+	`id_categoria` bigint(5) NOT NULL AUTO_INCREMENT,
+	`nome_categoria` varchar(255) NOT NULL UNIQUE,
+	`descricao` TEXT(255) NOT NULL,
+	`imagem` longblob NOT NULL,
+	PRIMARY KEY (`id_categoria`)
+);
+
+CREATE TABLE `tb_usuario` (
+	`id_usuario` bigint(5) NOT NULL AUTO_INCREMENT,
+	`nome` varchar(255) NOT NULL,
+	`user` varchar(255) NOT NULL UNIQUE,
+	`senha` varchar(255) NOT NULL,
+	PRIMARY KEY (`id_usuario`)
+);
+
+CREATE TABLE `tb_produto` (
+	`id_produto` bigint(5) NOT NULL AUTO_INCREMENT,
+	`nome_produto` varchar(255) NOT NULL UNIQUE,
+	`preco` DECIMAL(5,2) NOT NULL,
+	`fornecedor` varchar(255) NOT NULL,
+	`qtde_estoque` int(3) NOT NULL,
+	`categoria_id` bigint(5) NOT NULL,
+	PRIMARY KEY (`id_produto`)
+);
+
+ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk0` FOREIGN KEY (`categoria_id`) REFERENCES `tb_categoria`(`id_categoria`);
+
