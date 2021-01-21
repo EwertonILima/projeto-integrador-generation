e2df819f8fa179901cad198ce87d6c44b285fa85
jorgehauck <jorge.hauck1997@outlook.com>
2021-01-21 12:10:05 -0300

    Criação do Projeto E-commerce Agro

---
+++ madeInRoca/madeInRoca/src/test/java/com/proagro/madeInRoca/MadeInRocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proagro.madeInRoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeInRocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
