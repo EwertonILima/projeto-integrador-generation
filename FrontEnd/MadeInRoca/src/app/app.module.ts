5319c76816d76a5b74f1a90a4a124d27d5d41d68
igorgato <igormqg@gmail.com>
2021-02-19 11:54:51 -0300

    feito pagina login e cadastrar

--- FrontEnd/MadeInRoca/src/app/app.module.ts
+++ FrontEnd/MadeInRoca/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
