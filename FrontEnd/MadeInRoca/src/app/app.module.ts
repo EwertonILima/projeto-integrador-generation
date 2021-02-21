d2bf7d2b6f4414b7cec3fe26f42b95d4388d78c4
jorgehauck <jorge.hauck1997@outlook.com>
2021-02-21 00:45:01 -0300

    Implementação de Estilização na Página Produtos

--- FrontEnd/MadeInRoca/src/app/app.module.ts
+++ FrontEnd/MadeInRoca/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    ProdutosComponent,
+   
   ],
   imports: [
     BrowserModule,
