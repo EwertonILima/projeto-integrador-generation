984650a85054918ce42c37bfc84577fd81f3117f
Helloisa22 <helloisa.betriz44@gmail.com>
2021-02-25 23:48:30 -0300

    Conceitos Iniciais da page sobre

--- FrontEnd/MadeInRoca/src/app/app.module.ts
+++ FrontEnd/MadeInRoca/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
