f271e1348df0560f8996b83e1678f01d950d65a9
igorgato <igormqg@gmail.com>
2021-03-01 19:18:06 -0300

    input numerico adicionado

--- FrontEnd/MadeInRoca/src/app/cesta/cesta.component.ts
+++ FrontEnd/MadeInRoca/src/app/cesta/cesta.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CestaComponent implements OnInit {
 
+  nLegumes = 0
+  nFrutas = 0
+  nVerduras = 0
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  maxItens(){
+    if((this.nLegumes + this.nFrutas + this.nVerduras) > 12){
+      alert('Escolha no maximo 12 itens para compor sua cesta'),
+      this.nLegumes = 0 
+      this.nFrutas = 0 
+      this.nVerduras = 0
+    }
+    
   }
 
 }
