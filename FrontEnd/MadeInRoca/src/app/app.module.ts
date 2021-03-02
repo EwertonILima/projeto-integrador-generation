d33288bc793e5433374e79a8c02ad19decec4400
fernandaagapito2013@gmail.com <fernandaagapito2013@gmail.com>
2021-03-02 09:22:53 -0300

    Implementação do Navbar e rodapé

--- FrontEnd/MadeInRoca/src/app/app.module.ts
+++ FrontEnd/MadeInRoca/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
