a01d88e56cf874c4d3dfe2cca079616a6b9a95d7
fernandaagapito2013@gmail.com <fernandaagapito2013@gmail.com>
2021-03-03 10:52:57 -0300

    Realizado Get das pgs relacionadas aos produtos

--- FrontEnd/MadeInRoca/src/app/produto-shop/produto-shop.component.ts
+++ FrontEnd/MadeInRoca/src/app/produto-shop/produto-shop.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Produto } from '../model/Produto';
+import { ProdutoService } from '../service/produto.service';
 
 @Component({
   selector: 'app-produto-shop',
@@ -6,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-shop.component.css']
 })
 export class ProdutoShopComponent implements OnInit {
+  produto: Produto = new Produto()
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private produtoService: ProdutoService
+  ) { }
 
   ngOnInit(){
     window.scroll(0,0)
+    let id = this.route.snapshot.params['id']
+    this.findByIdProduto(id)
   }
 
+  findByIdProduto(id: number){
+    this.produtoService.getByIdProduto(id).subscribe((resp: Produto) =>{
+      this.produto = resp
+    })
+  }
 }
