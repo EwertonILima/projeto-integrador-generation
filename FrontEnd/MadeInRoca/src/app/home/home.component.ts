8b3617c5a3b2c09571455ac8425988e2020d5526
EwertonILima <ewertoninaciolima@gmail.com>
2021-03-06 19:56:39 -0300

    Inserção de textos, logica e rotas nos botões do home

--- FrontEnd/MadeInRoca/src/app/home/home.component.ts
+++ FrontEnd/MadeInRoca/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,9 @@ export class HomeComponent implements OnInit {
 
  
 
-  constructor() { }
+  constructor(
+    public auth: AuthService
+  ) { }
 
   ngOnInit() {
   }
