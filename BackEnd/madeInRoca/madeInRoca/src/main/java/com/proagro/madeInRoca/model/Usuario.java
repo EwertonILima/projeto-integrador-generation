79e4c54ca24852a271dc671d29407445dbb2351d
EwertonILima <ewertoninaciolima@gmail.com>
2021-03-10 18:15:49 -0300

    Alteração na quantidade de cacteres para o campo usuario, para caber emails maiores

--- BackEnd/madeInRoca/madeInRoca/src/main/java/com/proagro/madeInRoca/model/Usuario.java
+++ BackEnd/madeInRoca/madeInRoca/src/main/java/com/proagro/madeInRoca/model/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -31,7 +32,8 @@ public class Usuario {
 	private String tipoUsuario;
 
 	@NotNull
-	@Size(min = 3, max = 20)
+	@Email
+	@Size(min = 3, max = 50)
 	private String usuario;
 
 	@NotNull
